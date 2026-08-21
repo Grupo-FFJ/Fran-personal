bd2b31d7b2a53d5e906f5b64fa2196c05bb87fe1
Franco <francodaza819@gmail.com>
2026-08-20 21:17:51 -0300

    Asocia publicaciones con usuarios y creacion de repoPubli

---
+++ RepositorioPublicaciones.js
@@ -0,0 +1,24 @@
+export class RepositorioPublicaciones{
+    constructor(){
+        this.publicaciones = []
+    }
+    agregarPublicaciones(publicacion){
+        this.publicaciones.push(publicacion)
+    }
+    buscarPorUsuario(nombre){
+        const publiUser = this.publicaciones.filter(p => p.autor.nombre === nombre)
+        return publiUser
+    }
+    filtrarActivas(){
+        const publisActivas = this.publicaciones.forEach(p => p.activa === true)
+        return publisActivas
+    }
+    catidadTotal(){
+        let contador = 0
+        for(let i=0; i<this.publicaciones.length; i++){
+            contador = contador + 1
+        }
+        return contador
+    }
+}
+
--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 import { Publicaciones } from "./Publicaciones.js";
 import { Usuario } from "./Usuario.js"
-const publi1 = new Publicaciones("libro","matematica","franco")
-const publi2 = new Publicaciones("lapicera","color azul","fede")
-const publi3 = new Publicaciones("cuadernillo","cuadriculado","joaquin")
-const publi4 = new Publicaciones("lapiz","color rojo","luciana")
-const publi5 = new Publicaciones("cuaderno","rallado","franco")
+import { RepositorioPublicaciones } from "./RepositorioPublicaciones.js";
+// const publi1 = new Publicaciones("libro","matematica","franco")
+// const publi2 = new Publicaciones("lapicera","color azul","fede")
+// const publi3 = new Publicaciones("cuadernillo","cuadriculado","joaquin")
+// const publi4 = new Publicaciones("lapiz","color rojo","luciana")
+// const publi5 = new Publicaciones("cuaderno","rallado","franco")
 
-const publicaciones = [publi1,publi2,publi3,publi4,publi5]
+// const publicaciones = [publi1,publi2,publi3,publi4,publi5]
 
-publicaciones.forEach((Publicaciones) =>{
-    console.log(Publicaciones)
-})
+// publicaciones.forEach((Publicaciones) =>{
+//     console.log(Publicaciones)
+// })
 
-publi5.activa = false
-console.log(publi5)
+// publi5.activa = false
+// console.log(publi5)
 
-let contador = 0
-for(let i=0; i<publicaciones.length; i++){
-    if(publicaciones[i].activa === true){
-        contador = contador + 1
-        console.log(publicaciones[i].titulo)
-    }
-}
-console.log(contador)
+// let contador = 0
+// for(let i=0; i<publicaciones.length; i++){
+//     if(publicaciones[i].activa === true){
+//         contador = contador + 1
+//         console.log(publicaciones[i].titulo)
+//     }
+// }
+// console.log(contador)
 
-const publicacionesJSON = JSON.stringify(publicaciones, null, 2)
-console.log(publicacionesJSON)
+// const publicacionesJSON = JSON.stringify(publicaciones, null, 2)
+// console.log(publicacionesJSON)
 
 const user1 = new Usuario("Franco","fran@gmail.com")
 const user2 = new Usuario("fede","fede@gmail.com")
@@ -37,3 +38,25 @@ const publi8 = new Publicaciones("lapiz","color rojo",user3)
 const publi9 = new Publicaciones("cuaderno","rallado",user1)
 
 const nuevasPublicaciones = [publi6,publi7,publi8,publi9]
+
+nuevasPublicaciones.forEach((Publicaciones =>{
+    console.log(Publicaciones.mostrarResumen())
+}))
+
+const publiActiva = nuevasPublicaciones.filter(p => p.estaActiva())
+console.log(publiActiva)
+
+const primeraPubli = nuevasPublicaciones.find(p => p.autor.nombre === user1.nombre)
+console.log(primeraPubli)
+
+const repoPubli = new RepositorioPublicaciones()
+nuevasPublicaciones.forEach(p => repoPubli.agregarPublicaciones(p))
+console.log(repoPubli)
+
+// nuevasPublicaciones.forEach(p => {
+
+//     console.log(buscarUsuario)
+// })
+
+// // console.log(buscarUsuario)
+
