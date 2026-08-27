116b86df4c30c391e3b0d472a4911c78a3d2d649
Franco <francodaza819@gmail.com>
2026-08-27 20:34:14 -0300

    Agregando metodos

--- publicacion.js
+++ publicacion.js
@@ -14,4 +14,8 @@ export class Publicacion{
     estaActiva(){
         return this.activa
     }
+    diasPublicada(){
+        const ms = new Date() - this.fechaPublicacion
+        return Math.floor(ms / (1000 * 60 * 60 * 24))
+    }
 }
